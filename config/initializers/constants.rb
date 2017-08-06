a9f76b8dc4952a0dd7b7d0458a1f12e3a53509a2
Geoff Crittenden <geoffcritt@hotmail.com>
2017-08-06 14:34:46 -0500

    update constants for production deploy

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,10 +1,10 @@
-JOBS_URL               = DreamJob::CONFIG.job_info.base_url
-NO_JOBS_MESSAGE        = DreamJob::CONFIG.job_info.no_jobs_string
-JOBS_AVAILABLE_MESSAGE = DreamJob::CONFIG.job_info.jobs_avail_string
-SITE_DOWN_MESSAGE      = DreamJob::CONFIG.job_info.site_down_string
+JOBS_URL               = ENV['JOBS_URL']               || DreamJob::CONFIG.job_info.base_url
+NO_JOBS_MESSAGE        = ENV['NO_JOBS_MESSAGE']        || DreamJob::CONFIG.job_info.no_jobs_string
+JOBS_AVAILABLE_MESSAGE = ENV['JOBS_AVAILABLE_MESSAGE'] || DreamJob::CONFIG.job_info.jobs_avail_string
+SITE_DOWN_MESSAGE      = ENV['SITE_DOWN_MESSAGE']      || DreamJob::CONFIG.job_info.site_down_string
 
-TWILIO_ACCT_SID        = DreamJob::CONFIG.twilio.acct_sid
-TWILIO_AUTH_TOKEN      = DreamJob::CONFIG.twilio.auth_token
-FROM_PHONE_NUMBER      = DreamJob::CONFIG.twilio.phone_number
+TWILIO_ACCT_SID        = ENV['TWILIO_ACCT_SID']        || DreamJob::CONFIG.twilio.acct_sid
+TWILIO_AUTH_TOKEN      = ENV['TWILIO_AUTH_TOKEN']      || DreamJob::CONFIG.twilio.auth_token
+FROM_PHONE_NUMBER      = ENV['FROM_PHONE_NUMBER']      || DreamJob::CONFIG.twilio.phone_number
 
-TO_PHONE_NUMBER        = DreamJob::CONFIG.phone_info.phone_number
+TO_PHONE_NUMBER        = ENV['TO_PHONE_NUMBER']        || DreamJob::CONFIG.phone_info.phone_number
