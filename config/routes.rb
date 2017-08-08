bae783c3ae01fbeb7f9a9cf1272d792875087139
Geoff Crittenden <geoffcritt@hotmail.com>
2017-08-07 22:38:03 -0500

    add top level redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/', to: redirect('/api/v1/available_jobs')
+
   namespace :api do
     namespace :v1 do
       get '/status', to: 'api#status'
